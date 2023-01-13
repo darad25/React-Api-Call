9fcaf511acb7b103eef8e9e337cce1c503669841
1amdara <77836449+1amdara@users.noreply.github.com>
2023-01-13 01:39:39 -0800

    setup gh-pages

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -56,7 +56,7 @@ export const NavLink = styled(Link)`
 
 `;
 
-export const PlayerButton = styled(Button)`
+export const PlayerButton = styled(baseButton)`
    margin-left: auto;
    margin-top: 35px;
    border-radius: 100px;
